bb8326ad52e38eed727c1d05a92409758fe3177c
steven00237 <stevenekanga@gmail.com>
2026-09-18 15:25:09 +0100

    Port dynamique pour déploiement

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const https = require("https");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.API_KEY;
 
 app.use(express.static("."));
